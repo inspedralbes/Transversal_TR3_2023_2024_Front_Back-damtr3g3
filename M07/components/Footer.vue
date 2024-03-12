5a3e58fa98ed07e46c8c499d10810f243b09ec78
C0ural1 <albertcoralm@gmail.com>
2024-03-12 12:54:50 +0100

    Web oberta amb routes (nuxt)

---
+++ M07/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer>
+      <p>© 2024 Mi increíble aplicación Nuxt</p>
+      <!-- Agrega más contenido al pie de página según sea necesario -->
+    </footer>
+  </template>
---
+++ M07/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Inici</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </template>
