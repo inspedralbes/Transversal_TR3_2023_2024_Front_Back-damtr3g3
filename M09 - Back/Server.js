22f5856c32239b1ba0439caebd6ab6bb99dab9c1
C0ural1 <albertcoralm@gmail.com>
2024-03-16 19:37:17 +0100

    Sockets per les Sales y mongoDB functions per sales

--- M09 - Back/Server.js
+++ M09 - Back/Server.js
@@ -11,7 +11,7 @@ const http = require('http');
 const { spawn } = require('child_process');
 var session = require('express-session')
 const {getUsuarisLogin, registrarUsuariJoc} = require('../M06 - Acces BD/androidScript.js');
-const {crearSala, unirSala} = require('../M06 - Acces BD/mongo(Android).js');
+const {crearSala, unirSala, getInfoSalaConcreta} = require('../M06 - Acces BD/mongo(Android).js');
 const { v4: uuidv4 } = require('uuid');
 const PORT = 3327;
 const ubicacioGrafics = path.join(__dirname, "..", "M10/grafics");
@@ -57,8 +57,6 @@ app.post("/usuarisLogin", async function (req, res) {
   console.log(usuaris)
   for (var i = 0; i < usuaris.length && usuariTrobat == false; i++) {
       if (usuaris[i].nomCognoms == user.nomCognoms) {
-        console.log(usuaris[i].nomCognoms)
-        console.log(user.nomCognoms)
           const match = await bcrypt.compare(user.contrasenya, usuaris[i].contrasenya);
           if (match) {
               usuariTrobat = true;
@@ -127,9 +125,26 @@ app.post("/unirSala", async function (req, res) {
 
 //Sockets
 
+io.on('connection', (socket) => {
+  console.log('Usuari Connectat');
 
+  socket.on('disconnect', () => {
+      console.log('Usuari Desconnectat');
+  });
+  
+  socket.on('unirSala', async (idSala) => {
+    console.log('UnirSala emit rebut', idSala);
+    try {
+      const salaInfo = await getInfoSalaConcreta(idSala);
+      console.log('SalaInfo:', salaInfo); 
+      io.emit('actualitzarSala', salaInfo);
+      
+    } catch (error) {
+      console.error("Error al obtener la información de la sala:", error);
+    }
+  });
 
-
+});
 
 
 
