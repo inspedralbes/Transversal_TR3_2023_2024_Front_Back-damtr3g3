e923c0d3fb7312e7810fe75438ed60a6242da199
PanDyyG <a22angcamrui@inspedralbes.cat>
2024-03-20 12:57:52 +0100

    Broadcast y settings modificado

--- M07/pages/settings.vue
+++ M07/pages/settings.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
+  <div class="container">
     <h2>Configuración</h2>
-    <div>
+    <div class="input-container">
       <label for="velocidad-tronco">Velocidad del tronco:</label>
-      <input type="range" id="velocidad-tronco" min="0" max="100" step="1" style="background-color: #0074d9;" @input="updateVelocidadTronco">
+      <input type="range" id="velocidad-tronco" min="0" max="100" step="1" @input="updateVelocidadTronco">
       <span id="valor-velocidad-tronco">{{ velocidadTronco }}</span>
     </div>
-    <div>
+    <div class="input-container">
       <label for="empuje-tronco">Empuje del tronco:</label>
       <input type="number" id="empuje-tronco">
     </div>
-    <div>
+    <div class="input-container">
       <label for="salto">Salto:</label>
       <input type="number" id="salto">
     </div>
-    <div>
+    <div class="input-container">
       <label for="velocidad-personaje">Velocidad del personaje:</label>
       <input type="number" id="velocidad-personaje">
     </div>
@@ -26,12 +26,11 @@
 export default {
   data() {
     return {
-      velocidadTronco: 0 // Inicializamos la velocidad del tronco en 0
+      velocidadTronco: 0
     };
   },
   methods: {
     updateVelocidadTronco(event) {
-      // Actualizamos la velocidad del tronco con el valor actual del rango
       this.velocidadTronco = event.target.value;
     }
   }
@@ -39,34 +38,55 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f7f7f7;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
 h2 {
   font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.input-container {
+  margin-bottom: 15px;
 }
 
 label {
   font-size: 16px;
+  display: block;
+  margin-bottom: 5px;
 }
 
 input[type="range"],
 input[type="number"] {
   width: 100%;
-  padding: 5px;
+  padding: 10px;
   margin-top: 5px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
 }
 
 .custom-button {
-  background-color: #0074d9;
+  background-color: #0056a8; /* Color naranja vibrante */
   color: #fff;
   border: none;
-  padding: 10px 20px;
-  margin-top: 10px;
+  padding: 15px 30px;
+  margin-top: 20px;
   cursor: pointer;
   border-radius: 5px;
   font-weight: bold;
+  display: block;
+  margin: 0 auto;
   transition: background-color 0.3s ease;
 }
 
 .custom-button:hover {
-  background-color: #0056a8;
+  background-color: #022e57; /* Cambio de color al pasar el mouse */
 }
 </style>
