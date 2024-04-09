a43bdde4cc769acabff8c047f464ef1270397639
rubsersot <a22rubsersot@inspedralbes.cat>
2024-04-09 08:51:59 +0200

    Gràfics d'estadístiques

--- M06 - Acces BD/mongoStats.js
+++ M06 - Acces BD/mongoStats.js
@@ -1,4 +1,4 @@
-module.exports = {insertStats};
+module.exports = {insertStats, getStats};
 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb://admin:Pedralbes24@ac-tfqzwz8-shard-00-00.uzqtbce.mongodb.net:27017,ac-tfqzwz8-shard-00-01.uzqtbce.mongodb.net:27017,ac-tfqzwz8-shard-00-02.uzqtbce.mongodb.net:27017/?ssl=true&replicaSet=atlas-2n9o7w-shard-0&authSource=admin&retryWrites=true&w=majority&appName=AtlasCluster";
@@ -13,7 +13,16 @@ async function insertStats(data){
     } catch (error) {
         console.error("Error al insertar las estadísticas:", error);
         throw error;
-    } finally {
-        await client.close();
+    }
+}
+
+async function getStats(){
+    try {
+        await client.connect();
+        const result = await client.db("grup3").collection("stats").find({}).toArray();
+        return result;
+    } catch (error) {
+        console.error("Error al obtener las estadísticas:", error);
+        throw error;
     }
 }
---
+++ M07/pages/stats.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+  <div>
+    <canvas id="myChart2"></canvas>
+  </div>
+  <div>
+    <canvas id="myChart3"></canvas>
+  </div>
+</template>
+
+<script>
+import { getStats } from "../services/communicationsManager";
+import {
+  Chart,
+  LinearScale,
+  BarController,
+  CategoryScale,
+  BarElement,
+  ScatterController,
+  PointElement
+} from "chart.js";
+
+Chart.register(LinearScale, BarController, CategoryScale, BarElement, ScatterController, PointElement);
+
+export default {
+  data() {
+    return {
+      chart: null,
+      chart2: null,
+      stats: null,
+      playerPositions: {},
+      playerDamages: {},
+      playerData: [],
+      playerNames: [],
+    };
+  },
+  methods: {
+    processStats(stats) {
+      const playerPositions = {};
+      const playerDamages = {};
+      const playerCounts = {};
+      const playerNames = [];
+
+      stats.forEach((stat) => {
+        const numPlayers = stat.playerStats.length;
+        stat.playerStats.forEach((playerStat) => {
+          if (playerPositions[playerStat.playerName]) {
+            playerPositions[playerStat.playerName] += playerStat.position / numPlayers;
+            playerDamages[playerStat.playerName] += playerStat.damageReceived;
+            playerCounts[playerStat.playerName] += 1;
+          } else {
+            playerPositions[playerStat.playerName] = playerStat.position / numPlayers;
+            playerDamages[playerStat.playerName] = playerStat.damageReceived;
+            playerCounts[playerStat.playerName] = 1;
+            playerNames.push(playerStat.playerName);
+          }
+        });
+      });
+
+      const playerData = [];
+      for (const player in playerPositions) {
+        playerPositions[player] /= playerCounts[player];
+        playerDamages[player] /= playerCounts[player];
+        playerData.push({
+          x: playerPositions[player],
+          y: playerDamages[player],
+          playerName: player,
+        });
+      }
+
+      this.playerPositions = playerPositions;
+      this.playerDamages = playerDamages;
+      this.playerData = playerData;
+      this.playerNames = playerNames;
+    },
+    sortPlayersByPosition() {
+      return Object.entries(this.playerPositions)
+        .sort((a, b) => a[1] - b[1])
+        .reduce((obj, [key, val]) => ({ ...obj, [key]: val }), {});
+    },
+    sortPlayersByDamage() {
+      return Object.entries(this.playerDamages)
+        .sort((a, b) => a[1] - b[1])
+        .reduce((obj, [key, val]) => ({ ...obj, [key]: val }), {});
+    },
+  },
+  async mounted() {
+    this.stats = await getStats();
+    this.processStats(this.stats);
+
+    const sortedPlayerPositions = this.sortPlayersByPosition();
+    const sortedPlayerDamages = this.sortPlayersByDamage();
+
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const ctx2 = document.getElementById("myChart2").getContext("2d");
+    const ctx3 = document.getElementById('myChart3').getContext('2d');
+    if (this.chart) {
+      this.chart.destroy(); // destroy previous chart if it exists
+    }
+    if (this.chart2) {
+      this.chart2.destroy(); // destroy second chart if it exists
+    }
+    if (this.chart3) {
+      this.chart3.destroy(); // destroy third chart if it exists
+    }
+    this.chart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: Object.keys(sortedPlayerPositions),
+        datasets: [
+          {
+            label: "Posició Mitjana (Ajustada pel nombre de jugadors a la partida)",
+            data: Object.values(sortedPlayerPositions),
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            type: "linear",
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    this.chart2 = new Chart(ctx2, {
+      // add this block
+      type: "bar",
+      data: {
+        labels: Object.keys(sortedPlayerDamages),
+        datasets: [
+          {
+            label: "Cops Rebuts (Mitjana)",
+            data: Object.values(sortedPlayerDamages),
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            type: "linear",
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    this.chart3 = new Chart(ctx3, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Posició Mitjana vs Cops Rebuts (Mitjana)',
+          data: this.playerData.map((data, i) => ({ ...data, playerName: this.playerNames[i] })),
+          backgroundColor: 'rgba(153, 102, 255, 0.2)',
+          borderColor: 'rgba(153, 102, 255, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Posició Mitjana (Adjustada pel nombre de jugadors a la partida)'
+            }
+          },
+          y: {
+            type: 'linear',
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Cops rebuts (Mitjana)'
+            }
+          }
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              title: function(context) {
+                return context[0].raw.playerName;
+              },
+            }
+          }
+        }
+      }
+    });
+  },
+};
+</script>
+
+<style scoped></style>
