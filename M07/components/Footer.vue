2ba45924430dc95c42b07c707fd68f3fdd32edf9
Albert Coral <albert.coral@acceleralia.com>
2024-04-26 16:46:09 +0200

    prova commit

--- M07/components/Footer.vue
+++ M07/components/Footer.vue
@@ -2,7 +2,7 @@
     <div>
       <!-- Tu contenido principal aquí -->
       <footer class="bg-white text-black text-center p-4 fixed bottom-0 left-0 w-full">
-        <p>&copy; 2024 - Lava Rush</p>
+        <p>&copy; 2024 - LavaRush</p>
       </footer>
     </div>
   </template>
